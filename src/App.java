d6e25a2d000f4ffc05a463c267831fdfce6834f9
CaroGil <dcgil83@gmail.com>
2025-07-16 16:00:10 -0700

    First commit

---
+++ src/App.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+
+        /*
+         * if(condicion){
+         * instrucciones
+         * }else{
+         * instrucciones
+         * }
+         */
+
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Digite el numero: ");
+        int numero = entrada.nextInt();
+
+        // Implementar condicionales
+        if (numero >= 0) {
+            System.out.println("El numero " + numero + " es positivo");
+        } else {
+            System.out.println("El numero " + numero + " es negativo");
+        }
+    }
+}
